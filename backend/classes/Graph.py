1cc163031a4b5143e6c858081dba42bad0af13cf
Krzysztof5672 <82949285+Krzysztof5672@users.noreply.github.com>
2021-04-27 17:15:06 +0200

    Update Graph.py

--- backend/classes/Graph.py
+++ backend/classes/Graph.py
@@ -69,9 +69,95 @@ class Graph:
         :param edge_1: krawedz z klasy DetailedEdge
         :param edge_2: krawedz z klasy DetailedEdge
         :return: czy krawedzie spelniaja warunek nieprzecinania
-        TODO
+        zwraca True jesli krawedzie sie przecinaja i False jesli krawedzie sie nie przecinaja
+       
         """
-        return True
+        # przydaja mi sie takie rzeczy wiec na poczatek rzucilem 
+        x1 = min(edge_1.get_detailed_node_1().get_x_coord(), edge_1.get_detailed_node_2().get_x_coord() )
+        x2 = max(edge_1.get_detailed_node_1().get_x_coord(), edge_1.get_detailed_node_2().get_x_coord() )
+        y1 = min(edge_1.get_detailed_node_1().get_y_coord(), edge_1.get_detailed_node_2().get_y_coord() )
+        y2 = max(edge_1.get_detailed_node_1().get_y_coord(), edge_1.get_detailed_node_2().get_y_coord() )
+        
+        z1 = min(edge_2.get_detailed_node_1().get_x_coord(), edge_2.get_detailed_node_2().get_x_coord() )
+        z2 = max(edge_2.get_detailed_node_1().get_x_coord(), edge_2.get_detailed_node_2().get_x_coord() )
+        v1 = min(edge_2.get_detailed_node_1().get_y_coord(), edge_2.get_detailed_node_2().get_y_coord() )
+        v2 = max(edge_2.get_detailed_node_1().get_y_coord(), edge_2.get_detailed_node_2().get_y_coord() )
+        
+        # przypadek gdy obie krawedzie sa pionowe
+        if edge_1.get_detailed_node_1().get_x_coord() == edge_1.get_detailed_node_2().get_x_coord() and edge_2.get_detailed_node_1().get_x_coord() == edge_2.get_detailed_node_2().get_x_coord() :
+            if x1 == z1 and min(y2,v2) > max(y1,v1):
+                return True
+            else:
+                return False
+        # przypadek gdy pierwsza krawedz jest "pionowa" 
+        
+        if edge_1.get_detailed_node_1().get_x_coord() == edge_1.get_detailed_node_2().get_x_coord() :
+             if edge_2.get_detailed_node_1().get_y_coord() == edge_2.get_detailed_node_2().get_y_coord() :
+                 a2=0
+                 b2 = edge_2.get_detailed_node_1().get_y_coord()
+             else:
+                a2 = (edge_2.get_detailed_node_1().get_y_coord() - edge_2.get_detailed_node_2().get_y_coord() ) / (edge_2.get_detailed_node_1().get_x_coord() - edge_2.get_detailed_node_2().get_x_coord() )
+                b2 = edge_2.get_detailed_node_1().get_y_coord() - a2* edge_2.get_detailed_node_1().get_x_coord()
+            
+             x = edge_1.get_detailed_node_1().get_x_coord()
+             y = round(a2*x+b2,2)
+
+
+             if  y > y1 and y < y2 and y>=v1 and y<=v2 :
+                 print("Krawedzie  przecinaja sie w punkcie (",x,"," ,y,")")
+                 return True
+             else:
+                 return False            
+        
+       # przypadek gdy druga krawedz jest "pionowa" 
+        if edge_2.get_detailed_node_1().get_x_coord() == edge_2.get_detailed_node_2().get_x_coord() :
+            if edge_1.get_detailed_node_1().get_y_coord() == edge_1.get_detailed_node_2().get_y_coord() :
+                 a1=0
+                 b1 = edge_1.get_detailed_node_1().get_y_coord()
+            else:
+                a1 = (edge_1.get_detailed_node_1().get_y_coord() - edge_1.get_detailed_node_2().get_y_coord() ) / (edge_1.get_detailed_node_1().get_x_coord() - edge_1.get_detailed_node_2().get_x_coord() )
+                b1 = edge_1.get_detailed_node_1().get_y_coord() - a1* edge_1.get_detailed_node_1().get_x_coord()
+            
+            x = edge_2.get_detailed_node_1().get_x_coord()
+            y = round(a1*x+b1,2)
+       
+
+            if  y >= y1 and y <= y2 and y>v1 and y<v2 :
+                print("Krawedzie  przecinaja sie w punkcie (",x,"," ,y,")")
+                return True
+            else:
+                 return False
+    
+       # dla pozostalych przypadkow 
+           
+        "wspolczynniki na prosta przechodzaca przez edge1"
+        if edge_1.get_detailed_node_1().get_y_coord() == edge_1.get_detailed_node_2().get_y_coord() :
+            a1=0
+            b1 = edge_1.get_detailed_node_1().get_y_coord()
+        else:
+            a1 = (edge_1.get_detailed_node_1().get_y_coord() - edge_1.get_detailed_node_2().get_y_coord() ) / (edge_1.get_detailed_node_1().get_x_coord() - edge_1.get_detailed_node_2().get_x_coord() )
+            b1 = edge_1.get_detailed_node_1().get_y_coord() - a1* edge_1.get_detailed_node_1().get_x_coord()
+         
+        "wspolczynniki na prosta przechodzaca przez edge2"
+        if edge_2.get_detailed_node_1().get_y_coord() == edge_2.get_detailed_node_2().get_y_coord(): 
+            a2=0
+            b2 = edge_2.get_detailed_node_1().get_y_coord()
+        else:
+             a2 = (edge_2.get_detailed_node_1().get_y_coord() - edge_2.get_detailed_node_2().get_y_coord()) / (edge_2.get_detailed_node_1().get_x_coord() - edge_2.get_detailed_node_2().get_x_coord() )
+             b2 = edge_2.get_detailed_node_1().get_y_coord() - a2* edge_2.get_detailed_node_1().get_x_coord()
+        
+        if abs(a1-a2)<0.0001 and abs(b1-b2)<0.0001: 
+            return True
+        
+    #wspolrzedne punktu w ktorym sie proste przecinaja
+        x = round((b2-b1)/(a1-a2) ,2)
+        y = round(a2*x+b2,2)
+                      
+        if   x > x1 and x < x2 and y > y1 and y < y2 and x >z1 and x<z2 and y>v1 and y<v2 :
+            print("Krawedzie  przecinaja sie w punkcie (",x,"," ,y,")")
+            return True
+        else:
+            return False
 
     def set_nodes_from_file(self, file):
         """
