880ad784a883667f29c359fd89fac8e038fafa12
Jakub Bogdański <jakubbogd09@gmail.com>
2021-05-18 20:33:50 +0200

    filestests v3

--- unit_tests/FilesTests.py
+++ unit_tests/FilesTests.py
@@ -13,9 +13,6 @@ class TestFiles(unittest.TestCase):
     def test_read_cities_false_4(self):
         file="1.4.csv"
         self.assertFalse(files.read_cities(file))
-    def test_read_cities_false_5(self):
-        file="123456.csv"
-        self.assertFalse(files.read_cities(file))
     def test_read_cities_true(self):
         file="1.csv"
         self.assertTrue(files.read_cities(file))
@@ -31,9 +28,6 @@ class TestFiles(unittest.TestCase):
     def test_read_prod_false_4(self):
         file="2.4.csv"
         self.assertFalse(files.read_prod(file))
-    def test_read_prod_false_5(self):
-        file="123456.csv"
-        self.assertFalse(files.read_prod(file))
     def test_read_prod_true(self):
         file="2.csv"
         self.assertTrue(files.read_prod(file))
@@ -46,27 +40,29 @@ class TestFiles(unittest.TestCase):
     def test_read_worker_time_false_3(self):
         file="3.3.csv"
         self.assertFalse(files.read_worker_time(file))
-    def test_read_worker_time_false_4(self):
-        file="123456.csv"
-        self.assertFalse(files.read_worker_time(file))
     def test_read_worker_time_true(self):
         file="3.csv"
         self.assertTrue(files.read_worker_time(file))
     def test_read_solution_false_1(self):
         file="4.1.csv"
-        self.assertFalse(files.read_solution(file))
+        file1="1.csv"
+        file2="2.csv"
+        self.assertFalse(files.read_solution(file,file1,file2))
     def test_read_solution_false_2(self):
         file="4.2.csv"
-        self.assertFalse(files.read_solution(file))
+        file1="1.csv"
+        file2="2.csv"
+        self.assertFalse(files.read_solution(file,file1,file2))
     def test_read_solution_false_3(self):
         file="4.3.csv"
-        self.assertFalse(files.read_solution(file))
-    def test_read_solution_false_4(self):
-        file="123456.csv"
-        self.assertFalse(files.read_solution(file))
+        file1="1.csv"
+        file2="2.csv"
+        self.assertFalse(files.read_solution(file,file1,file2))
     def test_read_solution_true(self):
         file="4.csv"
-        self.assertTrue(files.read_solution(file))
+        file1="1.csv"
+        file2="2.csv"
+        self.assertTrue(files.read_solution(file,file1,file2))
         
     
 if __name__ == '__main__':
